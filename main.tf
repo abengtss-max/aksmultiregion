8b3df1a2f8828f72f333715fefb824742259fc2d
abengtss-max <ali_bengtsson@icloud.com>
2023-06-12 15:01:10 +0200

    source main.json from the local repository

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 #main.tf
 
-data "http" "aksc_release" {
-  url = "https://github.com/abengtss-max/aksmultiregion/blob/main/Assets/main.json"
-  request_headers = {
-    Accept = "application/json"
-    User-Agent = "request module"
-  }
+data "local_file" "release" {
+  filename = "Assets/main.json"
+}
+
+output "release" {
+  value = data.local_file.release.content
 }
 
 data "azurerm_client_config" "current" {}
@@ -19,7 +19,7 @@ resource "azurerm_resource_group_template_deployment" "aksc_deploy" {
   name = "AKS-C"
   resource_group_name = azurerm_resource_group.rg.name
   deployment_mode = "Incremental"
-  template_content = data.http.aksc_release.response_body
+  template_content = data.local_file.release.content
   parameters_content = jsonencode({
     resourceName = {value=var.resourceName}
     agentCount = {value=var.agentCount}
