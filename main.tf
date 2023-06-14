c24f55aa34733c9029c0ea50963653d9acf417ae
abengtss-max <ali_bengtsson@icloud.com>
2023-06-14 17:27:42 +0200

    added storage account name

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
   }
   backend "azurerm" {
       resource_group_name  = "tfstate"
-      storage_account_name = "tfstate18500"
+      storage_account_name = "<STORAGE_ACCOUNT_NAME>"
       container_name       = "tfstate"
       key                  = "terraform.tfstate"
   }
