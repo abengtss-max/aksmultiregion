98b6a8f6f33d299a6ad9a212d40f34df89a936a2
Christian Hase <christian.hase@initions-consulting.com>
2023-06-16 11:02:23 +0200

    Add redirect from root path

--- app/app.py
+++ app/app.py
@@ -1,15 +1,17 @@
 import time
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 import os
 
 app = Flask(__name__, template_folder='templates')
 
+@app.route('/')
+def hello():
+    return redirect("/index.html", code=302)
+
 @app.route('/index.html')
 def index():
     region = os.environ.get('REGION_NAME', 'unknown')
-    
     return render_template('index.html', response={'region': region})
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
